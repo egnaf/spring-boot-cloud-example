2cd2850d148dbed312bce64ac24859af77a64650
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-07-26 07:39:00 +0300

    Add service discovery 1

---
+++ service-discovery1/src/main/java/org/example/ServiceDiscovery1.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceDiscovery1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscovery1.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String home() {
+        return "Home page - Service Discovery 1";
+    }
+
+    @GetMapping(value = "/about")
+    public String about() {
+        return "About project - Service Discovery 1";
+    }
+}
